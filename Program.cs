ec784d56bd96b126282ebf9de0fbd8c6316f3c28
xxxnataliaxxx <112734090+xxxnataliaxxx@users.noreply.github.com>
2022-09-03 16:48:34 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+var o = new Octopus("Jack");
+
+Console.WriteLine(o.Name);  // Jack  
+// o.Legs = 20;                // Erro de compilação
+
+class Octopus
+{
+  public readonly string Name;
+  public readonly int Legs = 8;
+  
+  public Octopus(string name)
+  {
+    Name = name;
+  }
+}
